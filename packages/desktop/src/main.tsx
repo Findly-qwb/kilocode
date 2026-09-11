1c2c298aa61a3972a9bee0446c2a4d38154a61d3
邱文斌 <qiuwenbin@piscifun.com>
2026-09-11 22:58:11 +0800

    更新

--- packages/desktop/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "solid-js/web"
-import { connect } from "./client"
-import { App } from "./App"
-import { store } from "./store"
-import "./index.css"
-
-void connect().then(() => store.refresh())
-
-render(() => <App />, document.getElementById("root")!)
